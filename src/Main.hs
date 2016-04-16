50814f393fa3d0feecf22c952615733b5150f501
Charles Cooper <cooper.charles.m@gmail.com>
2016-04-16 11:57:36 -0400

    Improve examples

--- src/Main.hs
+++ src/Main.hs
@@ -1,23 +1,44 @@
 module Main where
 import Data.Map.Extensions as Map
+import Data.Map.Function
+import Data.Function
 
 type Name = String
 type Job = String
-type Age = Double
+type Age = Int
 
 fixture :: [(Name, Job, Age)]
 fixture = [ ("Alice", "Engineer", 25)
           , ("Bob"  , "Engineer", 35)
+          , ("Jane" , "Engineer", 28)
           , ("Ryan" , "Designer", 50)
           , ("Zoe"  , "Artist"  , 21)
           ]
 
-average :: (Fractional v) => Map k v -> v
-average xs = Map.foldr (+) 0 xs / fromIntegral (Map.size xs)
+average :: (Integral v, Fractional s) => Map k v -> s
+average xs = Map.foldr (+) 0 (fromIntegral <$> xs) / fromIntegral (Map.size xs)
 
 main :: IO ()
 main = do
   print $ fromList2 fixture 
+  putStrLn ""
+
   print $ transpose $ fromList2 fixture
+  putStrLn ""
+
   -- print the average age in a job
-  print $ fmap (average) $ transpose $ fromList2 fixture
+  fromList2 fixture
+    & transpose
+    <&> average
+    <&> round
+    & print
+  putStrLn ""
+
+  -- Find all occupations grouped by age
+  fromList2 fixture
+    <&> groupElemsBy (\age -> show (age `div` 10 * 10) ++ "'s")
+    & transpose
+    <&> Map.unions . Map.elems
+    & print
+  putStrLn ""
+
